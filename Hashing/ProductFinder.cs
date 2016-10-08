f89f10b4b3c844732ed5990db7522634051feef4
chnandakumarreddy <chnandakumarreddy@gmail.com>
2016-10-07 22:56:49 -0700

    Check if a product pair exists within the input array

---
+++ Hashing/ProductFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    public class ProductFinder
+    {
+        /// <summary>
+        /// CHecks if a pair exists within the array whose product equal to x
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public bool IsProductPairAvailable(List<int> input, int product)
+        {
+            HashSet<int> map = new HashSet<int>();
+            //If the input length is less than two, it means no pair can be found
+            if (input == null || input.Count() < 2)
+                return false;
+
+            for (int i = 0; i < input.Count(); i++)
+            {
+                //deal zero as a special case
+                if (product == 0 && input[i] == 0)
+                    return true;
+
+                //Add each item to the hashSet
+                map.Add(input[i]);
+
+                //checkif the current element is divsisble by zero,
+                //If it is divisible by zero, check if quotient is available inthe hashset
+                //if both conditions are true it means we have identitied the pair for product
+                if ((product % input[i]) == 0 && (map.Contains(product / input[i])))
+                    return true;
+
+            }
+            return false;
+        }
+    }
+}
--- Hashing/Program.cs
+++ Hashing/Program.cs
@@ -20,9 +20,14 @@ namespace Hashing
             //var input2 = new List<int> { 6, 3, -1, -3, 4, -2, 2, 4, 6, -12, -7 };
             //sZero.PrintOutput(sZero.GetSubArryasWithZeroSum(input2));
 
-            SubArrayWithSum saSum = new SubArrayWithSum();
-            var result = saSum.GetSubArrayWithSum(new List<int> { 1, 4, 20, 3, 10, 5 }, 33);
-            Console.WriteLine("["+result.StartIndex+", "+ result.EndIndex+"]");
+            //SubArrayWithSum saSum = new SubArrayWithSum();
+            //var result = saSum.GetSubArrayWithSum(new List<int> { 1, 4, 20, 3, 10, 5 }, 33);
+            //Console.WriteLine("["+result.StartIndex+", "+ result.EndIndex+"]");
+
+            ProductFinder pf = new ProductFinder();
+            Console.WriteLine(pf.IsProductPairAvailable(new List<int> { 10, 20, 9, 40 }, 400));
+            Console.WriteLine(pf.IsProductPairAvailable(new List<int> { 10, 20, 9, 40 }, 190));
+            Console.WriteLine(pf.IsProductPairAvailable(new List<int> { -10, 20, 9, 40 }, -400));
 
             Console.ReadLine();
         }
